177495bb27870d9c0a56a3f9ae7283860b21db7f
drodriguez97 <danrod.biz@gmail.com>
2022-11-07 18:39:32 -0800

    final commit

---
+++ Requests/FoodPicker.py
@@ -0,0 +1,34 @@
+from YelpApi import names , address 
+import random
+
+#create new list by using zip that contains both name and 
+new_list = zip(names, address)
+user_choice = ""
+keyword = "Yes"
+n = "No"
+
+locations = []
+for name,address in new_list:
+    locations.append((name , address))
+    
+
+while True:
+    user_choice = input("Would like to drink coffee today? Yes or No? ") 
+    
+    if user_choice == n:
+        print("Okay. No worries, come back later.")
+        break
+    if user_choice != keyword:
+        print("Please enter either Yes or No. Thank you.")
+        continue
+    else: 
+        location = random.choice(locations)
+        location = ('. '.join(str(x) for x in location)) #add period after location name
+        print(f'You should go to {location}. ')
+        break  
+    
+   
+    
+    
+
+         
---
+++ Requests/YelpApi.py
@@ -0,0 +1,56 @@
+#Business Search      URL -- 'https://api.yelp.com/v3/businesses/search'
+#Business Match       URL -- 'https://api.yelp.com/v3/businesses/matches'
+
+#Business Details     URL -- 'https://api.yelp.com/v3/businesses/{id}'
+#Business Reviews     URL -- 'https://api.yelp.com/v3/businesses/{id}/reviews'
+
+#import modules
+import requests
+from json import loads
+import json
+import creds
+
+#define business id
+
+business_id = creds.client_id
+
+#Define API_KEY, Endpoint, and Header **information that we send along to Yelp in order to successfully request their data**
+API_KEY = creds.api_key
+ENDPOINT = "https://api.yelp.com/v3/businesses/search"
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+#Define the parameters
+PARAMETERS = { 'term':'coffee',
+              'term': 'tea',
+              'term': 'cafes',
+                'limit':'50',
+                'radius': '12875', #8 miles
+                'location':'54 Kellog Drive Pomona,CA 91768' } 
+
+#Make a request to the yelp API
+response = requests.get(url = ENDPOINT, params= PARAMETERS, headers = HEADERS)
+
+#convert json string to a dictionary
+business_data = response.json()
+
+#create an empty list so we can add the biz names of the local cafes 
+names = []
+
+#iterate through key: business_data["businesses"] and collect value: biz["name"]   
+for biz in business_data["businesses"]:
+    name = biz["name"]
+    names.append(biz["name"])    
+
+
+address = []
+for biz in business_data["businesses"]:
+    addr = ( biz["location"]["display_address"] )
+    addr = (" ".join(str(x) for x in addr)) # add space between (street address) and (city, zip code)
+    address.append(addr)
+
+    
+#with open('.\\Requests\\results.txt', 'w', encoding='utf-8') as f:
+#    f.write(json.dumps(names))
+#    f.close()
+    
+    
